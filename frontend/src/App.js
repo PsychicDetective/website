60278de3e9c8ab6eab8a726463b97ecd6cb817da
MrGibby5257 <60327639+MrGibby5257@users.noreply.github.com>
2022-07-14 02:38:23 -0400

    commiting intial frontend and backend

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import { About, Footer, Header, Skills, Testimonial, Work } from './containers';
+import { NavBar } from './components';
+
+import './App.scss';
+const App = () => {
+  return (
+    <div className='app'>
+        <NavBar />
+        <Header />
+        <About />
+        <Work />
+        <Skills />
+        <Testimonial />
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
